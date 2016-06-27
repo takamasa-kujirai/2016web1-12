34587e49536d44efb3f278f57ca93f7ba2d7e667
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-06-27 17:34:19 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,10 +4,21 @@ document.getElementById('list').appendChild(li);
 
 document.getElementById('form').onsubmit = function(){
   window.alert(document.getElementById('form').number.value);
+  return false;
 };
+
+var Number=number;
+var message;
 
 for(var i=1; i<number; i++){
   var li = document.createElement('li');
+  if(i%3 === 0){
+    document.getElementById('Fizz');
+  }else if(i%5 === 0){
+    document.getElementById('Buzz');
+  }else{
   li.textContent = i + '回目';
   document.getElementById('list').appendChild(li);
+    
+  }
 }
