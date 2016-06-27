7137915c22f318d812bb4e0c7c196395621fff6c
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-06-27 17:49:36 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,8 +2,8 @@ var li =document.createElement('li');
 li.textContent = 'アイテム';
 document.getElementById('list').appendChild(li);
 
-document.getElementById('form').onsubmit = function(GX){
-//  window.alert(document.getElementById('form').number.value);
+document.getElementById('form').onsubmit = function(){
+window.alert(document.getElementById('form').number.value);
   //return false;
   
 };
