9688c43d8f7853544d14d260d0ba3f4c9d5c3db5
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-06-27 17:54:00 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -14,12 +14,15 @@ var Number=document.getElementById('form').number.value;
 for(var i=1; i<Number; i++){
   var li = document.createElement('li');
   if(i%3 === 0){
-    document.getElementById('Fizz');
+    //document.getElementById('Fizz');
+    message = 'Fizz'
   }else if(i%5 === 0){
-    document.getElementById('Buzz');
+    //document.getElementById('Buzz');
+    message = 'Buzz'
   }else{
-  li.textContent = i + '回目';
-  document.getElementById('list').appendChild(li);
-    
+  //li.textContent = i + '回目';
+  //document.getElementById('list').appendChild(li);
+  message= i + '回目';
   }
+  document.getElementById('choice').textContent = message;
 }
