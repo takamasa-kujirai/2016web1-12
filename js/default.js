eac40dfdca35465831aed9bd241787859a877601
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-06-27 18:12:54 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,15 +2,15 @@ var li =document.createElement('li');
 li.textContent = 'アイテム';
 document.getElementById('list').appendChild(li);
 
-document.getElementById('form').onsubmit = function(){
+//document.getElementById('form').onsubmit = function(){
 //window.alert(document.getElementById('form').number.value);
   //return false;
-  
-};
-
+//};
+var Number = parseInt(window.prompt('数を入力してえ'));
 
 var message;
-var Number=document.getElementById('form').number.value;
+//var Number=document.getElementById('form').number.value;
+
 for(var i=1; i<Number; i++){
   var li = document.createElement('li');
   if(i%15 === 0){
