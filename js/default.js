5af6cf35fe4a0e215d12bd84bf6d9d3c4f94cf46
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-06-27 17:14:40 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,9 @@
 var li =document.createElement('li');
 li.textContent = 'アイテム';
 document.getElementById('list').appendChild(li);
+
+for(var i=1; i<10; i++){
+  var li = document.createElement('li');
+  li.textContent = i + '回目';
+  document.getElementById('list').appendChild(li);
+}
